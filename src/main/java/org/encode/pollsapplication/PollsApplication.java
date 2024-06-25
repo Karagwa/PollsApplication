3384a2b74a1c6763afa8f7be59ebcd69919e9949
Karagwa <annkaragwa@gmail.com>
2024-06-25 16:07:55 +0300

    Updated the main file

--- src/main/java/org/encode/pollsapplication/PollsApplication.java
+++ src/main/java/org/encode/pollsapplication/PollsApplication.java
@@ -1,12 +1,11 @@
 package org.encode.pollsapplication;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
-
+import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
@@ -16,15 +15,12 @@ import java.util.TimeZone;
 })
 public class PollsApplication {
 
-
 	@PostConstruct
-	void init(){
+	void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollsApplication.class, args);
 	}
-
 }
